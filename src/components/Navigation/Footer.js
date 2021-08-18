05f0329560710cc431f8aa153178ec6722fd908e
Corridore <corridg@rose-hulman.edu>
2021-08-18 09:36:33 -0400

    added source link

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Container } from "reactstrap";
 
 class Footer extends React.Component {
+	redirect() {
+		window.open("https://github.com/Gabolicious/rc-website-web", "_blank")
+	}
+
 	render() {
 		return (
 			<footer style={Styles.footer}>
 				<Container>
-					<span className="text-muted" style={Styles.text}>made with <i className="fa fa-heart heart" /> by Gabo</span>
+					<span className="text-muted" style={Styles.text}>made with <i className="fa fa-heart heart" /> by Gabo. <a onClick={this.redirect} target="_blank" rel="noopener noreferrer" href="https://github.com/Gabolicious/rc-website-server">Source</a></span>
 				</Container>
 			</footer>
 		);
@@ -20,7 +24,7 @@ const Styles = {
 	footer: {
 		position: 'relative',
 		bottom: 0,
-		width: '80%',
+		width: '83%',
 		height: '60px',
 		lineHeight: '60px'
 	},
