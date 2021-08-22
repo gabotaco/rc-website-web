1d97932ffc0118c91eae3bcd35bf1a295dff96cd
Gabe Corridore <corridg@rose-hulman.edu>
2021-08-22 18:08:32 -0400

    refactored request

--- src/components/Profile/managers/cashout/PersonalVouchers.js
+++ src/components/Profile/managers/cashout/PersonalVouchers.js
@@ -1,57 +1,44 @@
 import React, {useEffect, useState} from 'react';
 import FormattedNumber from "../../../_common/FormattedNumber"
 import * as Api from "../../../../library/Api/api"
-import AppConfigs from "../../../../config/app_configs"
 import LoadingIcon from "../../../_presentational/LoadingIcon";
 
 const PersonalVouchers = (props) => {
     const [vouchers, setVouchers] = useState('LOADING');
 
-    function parseData(response) {
-        if (response.error === "Non-existant user") {
-            setVouchers('UNKNOWN')
-            return;
-        }
-
-        let pigsVouchers = 0;
-        let rtsVouchers = 0;
-
-        if (response.data.inventory.rts_voucher_air) {
-            rtsVouchers += response.data.inventory.rts_voucher_air.amount
-        }
-
-        if (response.data.inventory.rts_voucher) {
-            rtsVouchers += response.data.inventory.rts_voucher.amount
-        }
-
-        if (response.data.inventory.rts_voucher_heavy) {
-            rtsVouchers += response.data.inventory.rts_voucher_heavy.amount
-        }
-
-        if (response.data.inventory.pigs_voucher) {
-            pigsVouchers += response.data.inventory.pigs_voucher.amount
-        }
-
-        if (props.company === 'rts') {
-            setVouchers(rtsVouchers)
-        } else if (props.company === 'pigs') {
-            setVouchers(pigsVouchers)
-        } else {
-            setVouchers(rtsVouchers + pigsVouchers)
-        }
-    }
-
     useEffect(() => {
-        if (localStorage.getItem('data')) {
-            if (localStorage.getItem('data-time') && parseInt(localStorage.getItem('data-time')) > Date.now() - AppConfigs.tycoon_cooldown) {
-                return parseData(JSON.parse(localStorage.getItem('data')))
-            }
-        }
-
         Api.getTycoonData().then((response) => {
-            localStorage.setItem("data", JSON.stringify(response));
-            localStorage.setItem("data-time", Date.now());
-            return parseData(response)
+            if (response.error === "Non-existant user") {
+                setVouchers('UNKNOWN')
+                return;
+            }
+    
+            let pigsVouchers = 0;
+            let rtsVouchers = 0;
+    
+            if (response.data.inventory.rts_voucher_air) {
+                rtsVouchers += response.data.inventory.rts_voucher_air.amount
+            }
+    
+            if (response.data.inventory.rts_voucher) {
+                rtsVouchers += response.data.inventory.rts_voucher.amount
+            }
+    
+            if (response.data.inventory.rts_voucher_heavy) {
+                rtsVouchers += response.data.inventory.rts_voucher_heavy.amount
+            }
+    
+            if (response.data.inventory.pigs_voucher) {
+                pigsVouchers += response.data.inventory.pigs_voucher.amount
+            }
+    
+            if (props.company === 'rts') {
+                setVouchers(rtsVouchers)
+            } else if (props.company === 'pigs') {
+                setVouchers(pigsVouchers)
+            } else {
+                setVouchers(rtsVouchers + pigsVouchers)
+            }
         }).catch((err) => {
             console.error(err)
             setVouchers('UNKNOWN');
