4f22965f3ade89f308e0d7586f242d530d16ec0e
Gabe Corridore <corridg@rose-hulman.edu>
2022-05-10 20:17:47 -0400

    Used constant

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import { ApolloProvider as ApolloClientProvider} from '@apollo/client';
 import {ApolloProvider as ApolloHooksProvider} from 'react-apollo-hooks';
 import AuthLayout from "./layouts/Auth/AuthLayout";
 import AppLayout from "./layouts/App/AppLayout";
+import AppConfigs from './config/app_configs';
 // import "bootswatch/dist/darkly/bootstrap.min.css";
 // import "bootstrap/dist/css/bootstrap.css";
 const hist = createBrowserHistory();
 const cache = new InMemoryCache({addTypename: true});
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
+	uri: `${AppConfigs.server_url}/graphql`,
 	cache,
   	connectToDevTools: true, // we can remove this if we put a flag in .env saying we're in development mode
 	credentials: 'include'
