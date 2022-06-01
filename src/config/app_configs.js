aea7a143ce70c551e16e3eaea1ca3262b688df33
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2022-06-01 14:19:26 +0100

    update config

--- src/config/app_configs.js
+++ src/config/app_configs.js
@@ -1,6 +1,6 @@
 const AppConfigs = {
-	client_id: '976958674436251690',
-	server_url: 'https://api.gamerzs.me',
+	client_id: '705261074961662042',
+	server_url: 'http://localhost:4000',
 	app_name: 'RC',
 };
 
