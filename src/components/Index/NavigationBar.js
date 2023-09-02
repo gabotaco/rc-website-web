e5f2402478f63adbcfac99b3888d0263181a8bda
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2023-09-02 22:55:34 +0100

    Navigation - Convert to Bootstrap

--- src/components/Index/NavigationBar.js
+++ src/components/Index/NavigationBar.js
@@ -1,77 +1,124 @@
+import { Button } from 'reactstrap';
 import '../../assets/css/mobirise-icons.css';
 import '../../assets/css/social-icons.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
-const NavigationBar = () => {
+const NavigationBar = props => {
+	const HomePage = () => props.history.push('/');
+	const ProfilePage = () => props.history.push('/home/profile');
+	const TToolsPage = () => props.history.push('/home/ttools');
+	const ApplyPage = () => props.history.push('/home/apply');
+
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
 	return (
-		<>
-			<nav
-				className="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm transparent bg-color"
-				style={{ position: 'fixed', top: '0px', width: '100%', zIndex: 1 }}>
-				<button
-					className="navbar-toggler navbar-toggler-right"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<div className="hamburger">
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-					</div>
-				</button>
-				<div className="menu-logo">
-					<div className="navbar-brand">
-						<span className="navbar-logo">
-							<a href="home.html">
-								<img
-									src="assets/images/logo-rc-376x226.png"
-									alt="RC"
-									title=""
-									style={{ height: '4.9rem' }}
-								/>
-							</a>
-						</span>
-					</div>
-				</div>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
-						<li className="nav-item">
-							<a
-								className="nav-link link text-white display-4"
-								href="home/profile/">
-								<span
-									className="mbri-home mbr-iconfont mbr-iconfont-btn"
-									style={{ color: 'rgb(255, 134, 0)' }}></span>
-								Employee Login
-							</a>
-						</li>
-						<li className="nav-item">
-							<a
-								className="nav-link link text-white display-4"
-								href="home/ttools/">
-								<span
-									className="mobi-mbri mobi-mbri-sites mbr-iconfont mbr-iconfont-btn"
-									style={{ color: 'rgb(255, 134, 0)' }}></span>
-								TTools
-							</a>
-						</li>
-					</ul>
-					<div className="navbar-buttons mbr-section-btn">
-						<a className="btn btn-sm btn-primary display-4" href="home/apply">
+		<nav
+			className={
+				'navbar navbar-expand-lg fixed-top navbar-dark' +
+				(dropdownOpen ? ' bg-dark' : ' ml-5 mr-5')
+			}>
+			<a className="navbar-brand" onClick={HomePage} href="/">
+				<img
+					src="assets/images/logo-rc-376x226.png"
+					alt="RC"
+					title=""
+					style={{ height: '4.9rem' }}
+				/>
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				onClick={() => setDropdownOpen(!dropdownOpen)}>
+				<span
+					className="mbri-menu"
+					style={{
+						color: 'rgb(255, 134, 0)',
+					}}></span>
+			</button>
+			<div className="collapse navbar-collapse">
+				<ul className="navbar-nav mr-auto"></ul>
+				<form className="form-inline my-2 my-lg-0">
+					<a
+						onClick={ProfilePage}
+						className="nav-link link text-white font-weight-bold h5 d-flex"
+						href="home/profile/">
+						<span
+							className="mbri-home mbr-iconfont mbr-iconfont-btn mr-2"
+							style={{
+								color: 'rgb(255, 134, 0)',
+								fontSize: '1.6rem',
+							}}></span>
+						Employee Login
+					</a>
+					<a
+						onClick={TToolsPage}
+						href="home/ttools/"
+						className="nav-link link text-white font-weight-bold h5 mr-2 d-flex">
+						<span
+							className="mobi-mbri mbri-sites mbr-iconfont mbr-iconfont-btn mr-2"
+							style={{
+								color: 'rgb(255, 134, 0)',
+								fontSize: '1.6rem',
+							}}></span>
+						TTools
+					</a>
+					<Button
+						onClick={ApplyPage}
+						className="btn btn-sm btn-primary h5 pl-4 pr-4"
+						style={{
+							backgroundColor: '#ff8600',
+							borderColor: '#ff8600',
+							fontSize: '1.2rem',
+							borderRadius: '100px',
+						}}>
+						Apply Now!
+					</Button>
+				</form>
+			</div>
+			{dropdownOpen ? (
+				<div className="navbar-collapse">
+					<ul className="navbar-nav mr-auto"></ul>
+					<form className="form-inline my-2 my-lg-0">
+						<a
+							onClick={ProfilePage}
+							className="nav-link link text-white font-weight-bold h5 d-flex"
+							href="home/profile/">
 							<span
-								className="mbri-edit mbr-iconfont mbr-iconfont-btn"
-								style={{ color: 'rgb(255, 255, 255)' }}></span>
-							Apply Now!
+								className="mbri-home mbr-iconfont mbr-iconfont-btn mr-2"
+								style={{
+									color: 'rgb(255, 134, 0)',
+									fontSize: '1.6rem',
+								}}></span>
+							Employee Login
 						</a>
-					</div>
+						<a
+							onClick={TToolsPage}
+							href="home/ttools/"
+							className="nav-link link text-white font-weight-bold h5 mr-2 d-flex">
+							<span
+								className="mobi-mbri mbri-sites mbr-iconfont mbr-iconfont-btn mr-2"
+								style={{
+									color: 'rgb(255, 134, 0)',
+									fontSize: '1.6rem',
+								}}></span>
+							TTools
+						</a>
+						<Button
+							onClick={ApplyPage}
+							className="btn btn-sm btn-primary h5 pl-4 pr-4"
+							style={{
+								backgroundColor: '#ff8600',
+								borderColor: '#ff8600',
+								fontSize: '1.2rem',
+								borderRadius: '100px',
+							}}>
+							Apply Now!
+						</Button>
+					</form>
 				</div>
-			</nav>
-		</>
+			) : null}
+		</nav>
 	);
 };
 
